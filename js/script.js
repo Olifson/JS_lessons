c983d6c152b251c6af92da71f48b78c1b666c395
Dmitriy <olifson@mail.com>
2022-01-24 18:39:42 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+// alert('hello');
+
+// const result = confirm('are you here?');
+// console.log(result);
+
+// const answer = prompt('Вам есть 18?', '');
+// console.log(typeof(answer));
+
+// const answers = [];
+// answers[0] = prompt('Как Ваше имя?', '');
+// answers[1] = prompt('Как Ваша фамилия?', '');
+// answers[2] = prompt('Сколько Вам лет?', '');
+
+// console.log(typeof(answers));
+
+// const category = "toys";
+// console.log(`https://someurl.com/${category}/4`);
+
+// const user = 'Иван';
+// alert(`Привет, ${user}`);
+
+console.log('arr' + ' - object');
+
+let incr = 10,
+    decr = 10;
+
+    // incr++;
+    // decr--;
+
+    console.log(++incr);
+    console.log(--decr);
+
+   const isChecked = false,
+        isClose = false;
+
+    console.log(isChecked || !isClose);    
